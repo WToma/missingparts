634de1e7bfc8b112d614f3a2900f110d080da924
Tamas Weisz <weisztoma@gmail.com>
2020-02-04 20:32:50 -0800

    Implementation for game creation and game info retrieval

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,9 +1,47 @@
+use missingparts::cards::Card;
+use missingparts::gameplay::Gameplay;
+use serde::{Deserialize, Serialize};
+use std::sync::{Arc, Mutex};
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+    let games_mutex: Arc<Mutex<Vec<(Gameplay, Vec<Card>)>>> = Arc::new(Mutex::new(Vec::new()));
 
-    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+    let games_mutex_for_handler = Arc::clone(&games_mutex);
+    let get_game = warp::get()
+        .and(warp::path!("games" / usize))
+        .map(move |game_id| {
+            let id: usize = game_id;
+            let games: &Vec<(Gameplay, Vec<Card>)> = &games_mutex_for_handler.lock().unwrap();
+            let game_and_cards: &(Gameplay, Vec<Card>) = &games[id];
+            warp::reply::json(&game_and_cards.0.describe())
+        });
+
+    let games_mutex_for_handler = Arc::clone(&games_mutex);
+    let create_game = warp::post()
+        .and(warp::path!("games"))
+        .and(warp::body::content_length_limit(1024 * 16))
+        .and(warp::body::json())
+        .map(move |request: CreateGameRequest| {
+            let games: &mut Vec<(Gameplay, Vec<Card>)> =
+                &mut games_mutex_for_handler.lock().unwrap();
+            let new_index = games.len();
+            games.push(Gameplay::init(request.num_players));
+            warp::reply::json(&CreateGameResponse { id: new_index })
+        });
+
+    let games = get_game.or(create_game);
+
+    warp::serve(games).run(([127, 0, 0, 1], 3030)).await;
+}
+
+#[derive(Deserialize)]
+struct CreateGameRequest {
+    num_players: usize,
+}
+
+#[derive(Serialize)]
+struct CreateGameResponse {
+    id: usize,
 }
