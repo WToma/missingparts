55ba33f34d1c2ee9c5c9c8e04d9c462e26f8c917
Tamas Weisz <weisztoma@gmail.com>
2020-02-06 20:15:16 -0800

    Using ManagedGame type

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,13 +1,46 @@
+use missingparts::actionerror::ActionError;
 use missingparts::cards::Card;
-use missingparts::gameplay::Gameplay;
+use missingparts::gameplay::{GameDescription, Gameplay};
 use missingparts::playeraction::PlayerAction;
 use serde::{Deserialize, Serialize};
 use std::sync::{Arc, Mutex};
 use warp::Filter;
 
+struct ManagedGame {
+    gameplay: Gameplay,
+    secret_cards_per_player: Vec<Card>,
+}
+
+impl ManagedGame {
+    fn new(num_players: usize) -> ManagedGame {
+        let (gameplay, secret_cards_per_player) = Gameplay::init(num_players);
+        ManagedGame {
+            gameplay,
+            secret_cards_per_player,
+        }
+    }
+
+    fn describe(&self) -> GameDescription {
+        self.gameplay.describe()
+    }
+
+    fn get_private_card(&self, player_id: usize) -> Card {
+        self.secret_cards_per_player[player_id]
+    }
+
+    fn make_move(
+        &mut self,
+        player_id: usize,
+        player_action: PlayerAction,
+    ) -> Result<(), ActionError> {
+        self.gameplay
+            .process_player_action(player_id, player_action)
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    let games_mutex: Arc<Mutex<Vec<(Gameplay, Vec<Card>)>>> = Arc::new(Mutex::new(Vec::new()));
+    let games_mutex: Arc<Mutex<Vec<ManagedGame>>> = Arc::new(Mutex::new(Vec::new()));
 
     // TODO: the handler panic leaves the server in a zombie state
     //   need to handle panics within each handler!
@@ -17,9 +50,9 @@ async fn main() {
         .and(warp::path!("games" / usize))
         .map(move |game_id| {
             let id: usize = game_id;
-            let games: &Vec<(Gameplay, Vec<Card>)> = &games_mutex_for_handler.lock().unwrap();
-            let game_and_cards: &(Gameplay, Vec<Card>) = &games[id];
-            warp::reply::json(&game_and_cards.0.describe())
+            let games: &Vec<ManagedGame> = &games_mutex_for_handler.lock().unwrap();
+            let game_and_cards: &ManagedGame = &games[id];
+            warp::reply::json(&game_and_cards.describe())
         });
 
     let games_mutex_for_handler = Arc::clone(&games_mutex);
@@ -28,10 +61,9 @@ async fn main() {
         .and(warp::body::content_length_limit(1024 * 16))
         .and(warp::body::json())
         .map(move |request: CreateGameRequest| {
-            let games: &mut Vec<(Gameplay, Vec<Card>)> =
-                &mut games_mutex_for_handler.lock().unwrap();
+            let games: &mut Vec<ManagedGame> = &mut games_mutex_for_handler.lock().unwrap();
             let new_index = games.len();
-            games.push(Gameplay::init(request.num_players));
+            games.push(ManagedGame::new(request.num_players));
             let reply_body = warp::reply::json(&CreateGameResponse { id: new_index });
             warp::reply::with_status(
                 warp::reply::with_header(reply_body, "Location", format!("/games/{}", new_index)),
@@ -43,12 +75,10 @@ async fn main() {
     let get_private_card = warp::get()
         .and(warp::path!("games" / usize / "players" / usize / "private"))
         .map(move |game_id, player_id| {
-            let games: &Vec<(Gameplay, Vec<Card>)> = &games_mutex_for_handler.lock().unwrap();
-            let game_and_cards: &(Gameplay, Vec<Card>) = &games[game_id];
-            let cards: &Vec<Card> = &game_and_cards.1;
-            let player_card = &cards[player_id];
+            let games: &Vec<ManagedGame> = &games_mutex_for_handler.lock().unwrap();
+            let game_and_cards: &ManagedGame = &games[game_id];
             warp::reply::json(&PrivateCardResponse {
-                missing_part: *player_card,
+                missing_part: *&game_and_cards.get_private_card(player_id),
             })
         });
 
@@ -58,11 +88,9 @@ async fn main() {
         .and(warp::body::content_length_limit(1024 * 16))
         .and(warp::body::json())
         .map(move |game_id, player_id, player_action: PlayerAction| {
-            let games: &mut Vec<(Gameplay, Vec<Card>)> =
-                &mut games_mutex_for_handler.lock().unwrap();
-            let game_and_cards: &mut (Gameplay, Vec<Card>) = &mut games[game_id];
-            let game: &mut Gameplay = &mut game_and_cards.0;
-            let action_result = game.process_player_action(player_id, player_action);
+            let games: &mut Vec<ManagedGame> = &mut games_mutex_for_handler.lock().unwrap();
+            let game_and_cards: &mut ManagedGame = &mut games[game_id];
+            let action_result = game_and_cards.make_move(player_id, player_action);
             match action_result {
                 Ok(_) => warp::reply::with_status(
                     // TODO: use proper error handling instead. but that's hard to implement
