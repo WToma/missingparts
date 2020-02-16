57d2387837e0544cd1de833f2cb3a8d42cfed4ed
Tamas Weisz <weisztoma@gmail.com>
2020-02-15 18:50:08 -0800

    Using a RwLock instead of a Mutex

--- src/lobby.rs
+++ src/lobby.rs
@@ -1,7 +1,7 @@
 use crate::range_map::RangeMap;
 use crate::server_core_types::GameId;
 use std::cmp::min;
-use std::sync::Mutex;
+use std::sync::RwLock;
 
 pub trait GameCreator {
     /// Creates a new game with the specified number of players, and returns the ID of the game
@@ -31,32 +31,32 @@ enum LobbyPlayer {
 
 /// Manages the players who are waiting to join a game. Safe to access concurrently.
 pub struct Lobby {
-    internal: Mutex<NonThreadSafeLobby>,
+    internal: RwLock<NonThreadSafeLobby>,
 }
 
 impl Lobby {
     pub fn new() -> Lobby {
         Lobby {
-            internal: Mutex::new(NonThreadSafeLobby::new()),
+            internal: RwLock::new(NonThreadSafeLobby::new()),
         }
     }
 
     pub fn add_player(&self, min_game_size: usize, max_game_size: usize) -> PlayerIdInLobby {
         self.internal
-            .lock()
+            .write()
             .unwrap()
             .add_player(min_game_size, max_game_size)
     }
 
     /// Returns the game ID for the given player, if one has been assigned.
     pub fn get_player_game(&self, player_id: PlayerIdInLobby) -> Option<PlayerAssignedToGame> {
-        self.internal.lock().unwrap().get_player_game(player_id)
+        self.internal.read().unwrap().get_player_game(player_id)
     }
 
     /// Attempts to start a game for the players waiting in the lobby, respecting their game size
     /// preferences.
     pub fn start_games<T: GameCreator>(&self, game_manager: &T) {
-        self.internal.lock().unwrap().start_games(game_manager)
+        self.internal.write().unwrap().start_games(game_manager)
     }
 }
 
