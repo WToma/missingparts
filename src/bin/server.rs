0792fb79fc9e8bb7d0d89bc91e2aee886b9ba991
Tamas Weisz <weisztoma@gmail.com>
2020-02-23 15:50:57 -0800

    Passing the lobby to the handler

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,21 +1,43 @@
 use std::convert::Infallible;
 
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
 
-async fn hello(_: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new(Body::from("Hello World!")))
+use missingparts::lobby::{GameCreator, Lobby, PlayerAssignedToGame, PlayerIdInLobby};
+use missingparts::server_core_types::{GameId, Token, TokenVerifier};
+
+use std::sync::Arc;
+
+async fn missingparts_service(
+    lobby: Arc<Lobby>,
+    req: Request<Body>,
+) -> Result<Response<Body>, Infallible> {
+    if req.method() == &Method::POST && req.uri().path() == "lobby" {
+        unimplemented!()
+    } else {
+        Ok(Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(Body::empty())
+            .unwrap())
+    }
 }
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let lobby = Arc::new(Lobby::new());
+
     // For every connection, we must make a `Service` to handle all
     // incoming HTTP requests on said connection.
-    let make_svc = make_service_fn(|_conn| {
+    let make_svc = make_service_fn(move |_conn| {
         // This is the `Service` that will handle the connection.
         // `service_fn` is a helper to convert a function that
         // returns a Response into a `Service`.
-        async { Ok::<_, Infallible>(service_fn(hello)) }
+        let lobby = Arc::clone(&lobby);
+        async move {
+            Ok::<_, Infallible>(service_fn(move |req: Request<Body>| {
+                missingparts_service(Arc::clone(&lobby), req)
+            }))
+        }
     });
 
     let addr = ([127, 0, 0, 1], 3030).into();
